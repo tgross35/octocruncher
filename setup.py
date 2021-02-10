142484173b47b8a24417f17edf2310d80d416c44
Trevor Gross <tgross@intrepidcs.com>
2021-02-10 17:28:47 -0500

    Updated scripts for pypi

--- setup.py
+++ setup.py
@@ -4,14 +4,14 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="octocruncher", # Replace with your own username
+    name="octocruncher-v3", # Replace with your own username
     version="0.1.0",
     author="Trevor Gross",
     author_email="tgross@intrepidcs.com",
-    description="A simple package to implement the Octopart API",
+    description="A simple package to implement the Octopart API version 3",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/tgross35/octocruncher",
+    url="https://github.com/tgross35/octocruncher-v3",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
